f037ce4e6c96f75deea349665f045219fe787bf4
TeTedo <qkrxotjr9508@gmail.com>
2022-11-05 23:12:10 +0900

    RN연습

--- Weather/App.js
+++ Weather/App.js
@@ -1,12 +1,47 @@
-import { View } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import React from "react";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
-      <View style={{ flex: 1.5, backgroundColor: "yellow" }}></View>
-      <View style={{ flex: 1, backgroundColor: "green" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 88,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 2,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 130,
+  },
+  description: {
+    fontSize: 40,
+  },
+});
