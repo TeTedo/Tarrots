a54bcf70a21da356e68bf29b865a3697f91db3f4
Taesk <qkrxotjr9508@gmail.com>
2022-11-06 16:36:04 +0900

    weather

--- Weather/App.js
+++ Weather/App.js
@@ -1,18 +1,85 @@
-import { View, StyleSheet, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+const icons = {
+  Clouds: (
+    <MaterialCommunityIcons
+      name="weather-cloudy-alert"
+      size={24}
+      color="black"
+    />
+  ),
+};
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [ok, setOk] = useState(true);
+  const [days, setDays] = useState([]);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const respones = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await respones.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <View style={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </View>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <View style={{ display: "flex", flexDirection: "row" }}>
+              <Text style={styles.temp}>{parseFloat().toFixed(1)}</Text>
+              {icons.Clouds}
+            </View>
+          </View>
+        ) : (
+          days.map((v, idx) => (
+            <View style={styles.day}>
+              <View>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                {icons.Clouds}
+              </View>
+              <Text style={styles.description}>{day.weateher[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weateher[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -31,11 +98,9 @@ const styles = StyleSheet.create({
     fontSize: 88,
     fontWeight: "500",
   },
-  weather: {
-    flex: 2,
-  },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
@@ -43,5 +108,8 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 40,
+  },
+  tinyText: {
+    fontSize: 20,
   },
 });
