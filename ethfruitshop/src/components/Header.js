fe7866a755a19a726a5530f33028a87cef4cee31
Taesk <qkrxotjr9508@gmail.com>
2022-12-02 00:04:09 +0900

    기능 구현 완료

--- ethfruitshop/src/components/Header.js
+++ ethfruitshop/src/components/Header.js
@@ -4,19 +4,45 @@ import { DeployedContext } from "../App";
 const Header = () => {
   const [register, setRegister] = useState(false);
   const [balance, setBalance] = useState();
-  const { web3, account } = useContext(DeployedContext);
+  const { web3, account, deployed } = useContext(DeployedContext);
+  const [hasFruit, setHasFruit] = useState({});
   useEffect(() => {
+    if (!account) return;
     const init = async () => {
-      const balance = await web3?.eth.getBalance(account);
+      const balance = await web3.eth.getBalance(account);
       setBalance((balance / 10 ** 18).toFixed(2));
     };
     init();
   }, [account]);
+  useEffect(() => {
+    (async () => {
+      if (!deployed) return;
+      const getHasFruit = await deployed.methods
+        .hasFruit()
+        .call({ from: account });
+      const getFruitWallet = await deployed.methods
+        .getFruitWallet()
+        .call({ from: account });
+      let temp = {};
+      for (let i = 0; i < getHasFruit.length; i++) {
+        temp[getHasFruit[i]] = getFruitWallet[i];
+      }
+      setHasFruit(temp);
+    })();
+  }, [deployed]);
   return (
     <div className="Header">
       <div>주소 : {account}</div>
       <div>잔액 : {balance}ETH</div>
-      <div>보유 과일 : </div>
+      <div>
+        보유 과일
+        <div></div>
+        {Object.keys(hasFruit).map((v, idx) => (
+          <div key={idx}>
+            {v} : {hasFruit[v]}
+          </div>
+        ))}
+      </div>
       <button
         onClick={() => {
           setRegister(!register);
